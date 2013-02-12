2de7044a9a4853d5677d2dceb6c8b2eec9b62781
marius <marius.cobzarenco@olivetree-solutions.com>
2013-02-12 17:31:42 +0000

    new strategy

--- harb/strategy.py
+++ harb/strategy.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 
 from common import TO_BE_PLACED
-from analytics import HorseModel, RiskModel2
+from analytics import HorseModel
 import risk
 
 WARN_LIQUIDITY = 0.2
@@ -22,22 +22,29 @@ class Strategy(object):
 
         self._bets = []
 
-    def bet(self, event_id, selection, amount):
+    def bet(self, event_id, selection, amount, user_fields=None):
         assert event_id == self._curr['event_id']
+        if user_fields is None:
+            user_fields = {}
 
         odds = self.vwao.get_value((event_id, selection), 'vwao')
         win = int(selection in self._curr['winners'])
         pnl = win * amount * (odds - 1) - (1 - win) * amount
 
-        self._bets.append({'event_id' : event_id,
-                           'selection': selection,
-                           'amount': amount,
-                           'odds': odds,
-                           'pnl': pnl,
-                           'win': self._curr['winners']})
+        bet = {'event_id' : event_id,
+               'selection': selection,
+               'amount': amount,
+               'odds': odds,
+               'pnl': pnl,
+               'win': self._curr['winners']}
+        bet.update(map(lambda x: ('user_' + x[0], x[1]), user_fields.items()))
+        self._bets.append(bet)
 
     def handle_race(self, race):
         raise RuntimeError('Abstract base class: implement the function')
+
+    def total_matched(self, event_id):
+        return self._total_matched.get_value(event_id)
 
     def run(self, country=None, start_date=None, end_date=None):
         where_clause = defaultdict(lambda: {})
@@ -46,8 +53,9 @@ class Strategy(object):
         if end_date is not None:
             where_clause['scheduled_off']['$lte'] = end_date
 
-        self.vwao = pd.DataFrame(list(db[self.vwao_coll].find(where_clause, sort=[('scheduled_off', 1)]))) \
-            .set_index(['event_id', 'selection'])
+        self.vwao = pd.DataFrame(list(db[self.vwao_coll].find(where_clause, sort=[('scheduled_off', 1)])))
+        self._total_matched = self.vwao.groupby('event_id')['volume_matched'].sum()
+        self.vwao = self.vwao.set_index(['event_id', 'selection'])
 
         if country is not None:
             where_clause['country'] = country
@@ -66,12 +74,11 @@ class Strategy(object):
                 start_time = time.clock()
 
 
-
 class Jockey(Strategy):
     def __init__(self, db, vwao='vwao', train='train'):
         super(Jockey, self).__init__(db, vwao, train)
 
-        self.hm = HorseModel(beta=10.0, tau=2.0)
+        self.hm = HorseModel()
 
     def handle_race2(self, race):
         if race['event'] != TO_BE_PLACED:
@@ -88,17 +95,26 @@ class Jockey(Strategy):
             return
 
         runners = race['selection']
-        if np.all(self.hm.get_runs(runners) > 0): #race['scheduled_off'] >= datetime.datetime(2012, 1, 3):
+        # self.total_matched(race['event_id']) > 2e5
+        if np.all(self.hm.get_runs(runners) > 2):
             vwao = self.vwao.ix[race['event_id']]['vwao'][runners].values
             q = 1.0 / vwao / np.sum(1.0 / vwao)
-            p = self.hm.pwin(runners)
+            p = self.hm.pwin_trapz(runners)
+
+            rel = p / q - 1.0
+            t = 0.1
+
+            p[rel < -t] = q[rel < -t] * 0.9
+            p[rel > t] = q[rel > t] * 1.1
+
+            #print(p)
             # ps = (self.hm.get_runs(runners) * p + 4 * q) / (4 + self.hm.get_runs(runners))
 
             #w = RiskModel2(p, q).optimal_w()
             w = risk.nwin1_l2reg(p, q, 0.2)
 
             logging.info('Placing some bets: %.2f' % np.sum(np.abs(w)))
-            [self.bet(race['event_id'], r, -w[i]) for i, r in enumerate(runners)]
+            [self.bet(race['event_id'], r, w[i], {'p': p[i]}) for i, r in enumerate(runners)]
 
         self.hm.fit_race(race)
 
@@ -114,16 +130,16 @@ if __name__ == '__main__':
 
     configure_root_logger(True)
 
-    db = MongoClient(port=33000)['betfair']
+    db = MongoClient(port=30001)['betfair']
     algo = Jockey(db)
 
     st = time.clock()
-    algo.run('GB', datetime.datetime(2012, 1, 1), datetime.datetime(2013, 6, 1))
+    algo.run('GB', datetime.datetime(2012, 1, 1), datetime.datetime(2013, 1, 1))
     en = time.clock()
 
     df = pd.DataFrame.from_dict(algo._bets)
     print(df.to_string())
     print('Done in %.4f s' % (en - st))
 
-    df.save('/home/marius/playground/betfair-trading/out/back4.pd')
+    df.save('/home/marius/playground/btrading/back3.pd')
 
