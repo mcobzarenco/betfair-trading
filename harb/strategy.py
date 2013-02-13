a2fb349e9af0202ab2e444bb2e8b609ab33c99aa
marius <marius.cobzarenco@olivetree-solutions.com>
2013-02-13 15:43:57 +0000

    some basic scorecard functionality

--- harb/strategy.py
+++ harb/strategy.py
@@ -7,11 +7,12 @@ import time
 import numpy as np
 import pandas as pd
 
-from common import TO_BE_PLACED
+from common import TO_BE_PLACED, pandas_to_dicts
 from analytics import HorseModel
 import risk
 
 WARN_LIQUIDITY = 0.2
+DEFAULT_COMM = 0.95
 
 
 class Strategy(object):
@@ -19,7 +20,6 @@ class Strategy(object):
         self.vwao_coll = vwao
         self.train_coll = train
         self.db = db
-
         self._bets = []
 
     def bet(self, event_id, selection, amount, user_fields=None):
@@ -31,7 +31,8 @@ class Strategy(object):
         win = int(selection in self._curr['winners'])
         pnl = win * amount * (odds - 1) - (1 - win) * amount
 
-        bet = {'event_id' : event_id,
+        bet = {'event_id': event_id,
+               'scheduled_off': self._curr['scheduled_off'],
                'selection': selection,
                'amount': amount,
                'odds': odds,
@@ -42,9 +43,6 @@ class Strategy(object):
 
     def handle_race(self, race):
         raise RuntimeError('Abstract base class: implement the function')
-
-    def total_matched(self, event_id):
-        return self._total_matched.get_value(event_id)
 
     def run(self, country=None, start_date=None, end_date=None):
         where_clause = defaultdict(lambda: {})
@@ -73,11 +71,48 @@ class Strategy(object):
                              % (i, time.clock() - start_time, len(self._bets), pnl))
                 start_time = time.clock()
 
+    def get_total_matched(self, event_id):
+        return self._total_matched.get_value(event_id)
+
+    def get_bets(self):
+        return self._bets
+
+
+def make_scorecard(strategy, percentile_width=60, comm=DEFAULT_COMM):
+    def calculate_collateral(group):
+        return np.min(risk.nwin1_bet_returns(group.amount.values, group.odds.values))
+
+    bets_summary = ['amount', 'pnl', 'odds']
+    bets = pd.DataFrame.from_dict(strategy.get_bets())
+
+    events = pd.DataFrame.from_dict([{'event_id': k,
+                                      'pnl_gross': v.pnl.sum(),
+                                      'coll': calculate_collateral(v),
+                                      'scheduled_off': v['scheduled_off'].iget(0)}
+                                    for k, v in bets.groupby('event_id')]).set_index('event_id')
+    events['pnl_net'] = events.pnl_gross
+    events['pnl_net'][events.pnl_net > 0] *= comm
+
+    daily_pnl = events[['scheduled_off', 'pnl_gross', 'pnl_net']]
+    daily_pnl['scheduled_off'] = daily_pnl['scheduled_off'].map(lambda t: datetime.datetime(t.year, t.month, t.day))
+    daily_pnl = daily_pnl.groupby('scheduled_off').sum().rename({'pnl_gross': 'gross', 'pnl_net': 'net'})
+    daily_pnl['gross_cumm'] = daily_pnl['gross'].cumsum()
+    daily_pnl['bet_cumm'] = daily_pnl['net'].cumsum()
+
+    scorecard = {
+        'all': bets[bets_summary].describe(percentile_width).to_dict(),
+        'backs': bets[bets['amount'] > 0][bets_summary].describe(percentile_width).to_dict(),
+        'lays': bets[bets['amount'] < 0][bets_summary].describe(percentile_width).to_dict(),
+        'events': events.describe(percentile_width).to_dict(),
+        'pnl': pandas_to_dicts(events[['gross', 'net']])
+    }
+
+    return scorecard, events
+
 
 class Jockey(Strategy):
     def __init__(self, db, vwao='vwao', train='train'):
         super(Jockey, self).__init__(db, vwao, train)
-
         self.hm = HorseModel()
 
     def handle_race2(self, race):
@@ -98,7 +133,8 @@ class Jockey(Strategy):
         # self.total_matched(race['event_id']) > 2e5
         if np.all(self.hm.get_runs(runners) > 2):
             vwao = self.vwao.ix[race['event_id']]['vwao'][runners].values
-            q = 1.0 / vwao / np.sum(1.0 / vwao)
+            #q = 1.0 / vwao / np.sum(1.0 / vwao)
+            q = 1.0 / vwao
             p = self.hm.pwin_trapz(runners)
 
             rel = p / q - 1.0
@@ -111,10 +147,14 @@ class Jockey(Strategy):
             # ps = (self.hm.get_runs(runners) * p + 4 * q) / (4 + self.hm.get_runs(runners))
 
             #w = RiskModel2(p, q).optimal_w()
-            w = risk.nwin1_l2reg(p, q, 0.2)
+            w = risk.nwin1_l2reg(p, q, 0.1)
 
-            logging.info('Placing some bets: %.2f' % np.sum(np.abs(w)))
-            [self.bet(race['event_id'], r, w[i], {'p': p[i]}) for i, r in enumerate(runners)]
+            coll = np.min(risk.nwin1_bet_returns(w, 1 / q))
+            if coll > -60:
+                logging.info('Placing some bets: %.2f' % np.sum(np.abs(w)))
+                [self.bet(race['event_id'], r, w[i], {'p': p[i]}) for i, r in enumerate(runners)]
+            else:
+                logging.info('Skipping placing bets as coll=%.2f' % coll)
 
         self.hm.fit_race(race)
 
@@ -141,5 +181,5 @@ if __name__ == '__main__':
     print(df.to_string())
     print('Done in %.4f s' % (en - st))
 
-    df.save('/home/marius/playground/btrading/back3.pd')
+    df.save('/home/marius/playground/btrading/back5.pd')
 
