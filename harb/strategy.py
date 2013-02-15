06aa6ec7e479e3ed1ff248a1ce753fd39ef7697e
marius <marius.cobzarenco@olivetree-solutions.com>
2013-02-15 17:25:10 +0000

    upload script for the horseracebase dataset

--- harb/strategy.py
+++ harb/strategy.py
@@ -38,8 +38,9 @@ class Strategy(object):
                'amount': amount,
                'odds': odds,
                'pnl': pnl,
-               'win': self._curr['winners']}
-        bet.update(map(lambda x: ('user_' + x[0], x[1]), user_fields.items()))
+               'selection_won': win,
+               'winners': self._curr['winners']}
+        bet.update(map(lambda x: ('u_' + x[0], x[1]), user_fields.items()))
         self._bets.append(bet)
 
     def handle_race(self, race):
@@ -55,6 +56,10 @@ class Strategy(object):
         self.vwao = pd.DataFrame(list(db[self.vwao_coll].find(where_clause, sort=[('scheduled_off', 1)])))
         self._total_matched = self.vwao.groupby('event_id')['volume_matched'].sum()
         self.vwao = self.vwao.set_index(['event_id', 'selection'])
+
+        # Calculate the probabilities implied by the odds and a uniform distribution
+        self.vwao['implied'] = self.vwao.groupby(level=0)['vwao'].transform(lambda x: (1.0 / x) / (1.0 / x).sum())
+        self.vwao['uniform'] = self.vwao.groupby(level=0)['vwao'].transform(lambda x: 1.0 / len(x))
 
         if country is not None:
             where_clause['country'] = country
@@ -78,58 +83,80 @@ class Strategy(object):
     def get_bets(self):
         return self._bets
 
+    @staticmethod
+    def _log_likelihood(won, prob):
+        return np.log(won * prob + (1 - won) * (1.0 - prob))
 
-def make_scorecard(strategy, percentile_width=60, comm=DEFAULT_COMM, jsonify=True):
-    def calculate_collateral(group):
-        return np.min(risk.nwin1_bet_returns(group.amount.values, group.odds.values))
-
-    bets_summary = ['amount', 'pnl', 'odds']
-    bets = pd.DataFrame.from_dict(strategy.get_bets())
-
-    events = pd.DataFrame.from_dict([{'event_id': k,
-                                      'pnl_gross': v.pnl.sum(),
-                                      'coll': calculate_collateral(v),
-                                      'scheduled_off': v['scheduled_off'].iget(0)}
-                                    for k, v in bets.groupby('event_id')]).set_index('event_id')
-    events['pnl_net'] = events.pnl_gross
-    events['pnl_net'][events.pnl_net > 0] *= comm
-
-    daily_pnl = events[['scheduled_off', 'pnl_gross', 'pnl_net']]
-    daily_pnl['scheduled_off'] = daily_pnl['scheduled_off'].map(lambda t: datetime.datetime(t.year, t.month, t.day))
-    daily_pnl = daily_pnl.groupby('scheduled_off').sum().rename(columns={'pnl_gross': 'gross', 'pnl_net': 'net'})
-    daily_pnl['gross_cumm'] = daily_pnl['gross'].cumsum()
-    daily_pnl['net_cumm'] = daily_pnl['net'].cumsum()
-
-    scorecard = {
-        'all': bets[bets_summary].describe(percentile_width),
-        'backs': bets[bets['amount'] > 0][bets_summary].describe(percentile_width),
-        'lays': bets[bets['amount'] < 0][bets_summary].describe(percentile_width),
-        'events': events.describe(percentile_width),
-        'daily_pnl': daily_pnl
-    }
-
-    if jsonify:
+    @staticmethod
+    def _jsonify_scorecard(scorecard):
         for col in ['all', 'backs', 'lays', 'events']:
             scorecard[col] = scorecard[col].to_dict()
         scorecard['daily_pnl'] = list(pandas_to_dicts(scorecard['daily_pnl'].reset_index()))
 
-    return scorecard
+    def make_scorecard(self, percentile_width=60, comm=DEFAULT_COMM, jsonify=True, llik_frame=False):
+        def calculate_collateral(group):
+            return np.min(risk.nwin1_bet_returns(group.amount.values, group.odds.values))
+
+        bets_summary = ['amount', 'pnl', 'odds']
+        bets = pd.DataFrame.from_dict(self.get_bets())
+        user_columns = bets.filter(regex='u_.*').columns.tolist()
+        llik = bets.groupby(['event_id', 'selection'])[['selection_won'] + user_columns].last() \
+            .join(self.vwao[['implied', 'uniform']])
+        llik['llik_implied'] = Strategy._log_likelihood(llik['selection_won'], llik['implied'])
+        llik['llik_uniform'] = Strategy._log_likelihood(llik['selection_won'], llik['uniform'])
+
+        events = pd.DataFrame.from_dict([{'event_id': k,
+                                          'pnl_gross': v.pnl.sum(),
+                                          'coll': calculate_collateral(v),
+                                          'scheduled_off': v['scheduled_off'].iget(0)}
+                                        for k, v in bets.groupby('event_id')]).set_index('event_id')
+        events['pnl_net'] = events.pnl_gross
+        events['pnl_net'][events.pnl_net > 0] *= comm
+
+        daily_pnl = events[['scheduled_off', 'pnl_gross', 'pnl_net']]
+        daily_pnl['scheduled_off'] = daily_pnl['scheduled_off'].map(lambda t: datetime.datetime(t.year, t.month, t.day))
+        daily_pnl = daily_pnl.groupby('scheduled_off').sum().rename(columns={'pnl_gross': 'gross', 'pnl_net': 'net'})
+        daily_pnl['gross_cumm'] = daily_pnl['gross'].cumsum()
+        daily_pnl['net_cumm'] = daily_pnl['net'].cumsum()
+
+        scorecard = {
+            'all': bets[bets_summary].describe(percentile_width),
+            'backs': bets[bets['amount'] > 0][bets_summary].describe(percentile_width),
+            'lays': bets[bets['amount'] < 0][bets_summary].describe(percentile_width),
+            'events': events.describe(percentile_width),
+            'daily_pnl': daily_pnl,
+            'llik': {
+                'implied': llik['llik_implied'].sum(),
+                'uniform': llik['llik_uniform'].sum()
+            }
+        }
+
+        if jsonify:
+            Strategy._jsonify_scorecard(scorecard)
+
+        if llik_frame:
+            scorecard['_frames'] = {'llik': llik}
+
+        return scorecard
 
 
-class Jockey(Strategy):
+class Balius(Strategy):
     def __init__(self, db, vwao='vwao', train='train'):
-        super(Jockey, self).__init__(db, vwao, train)
-        self.hm = HorseModel()
+        super(Balius, self).__init__(db, vwao, train)
+        self.hm = HorseModel(tau=1, beta=20, draw_probability=0.001)
 
-    def handle_race2(self, race):
-        if race['event'] != TO_BE_PLACED:
-            return
-        #logging.info('To be placed event (event_id = %d)' % race['event_id'])
-        try:
-            vwao = self.vwao.ix[race['event_id']]['vwao']
-            self.bet(race['event_id'], vwao[vwao == vwao.min()].index[0], 2.0)
-        except KeyError:
-            logging.warn('No VWAO for %d' % race['event_id'])
+    def make_scorecard(self, percentile_width=60, comm=DEFAULT_COMM, jsonify=True, llik_frame=False):
+        scorecard = super(Balius, self).make_scorecard(percentile_width, comm, jsonify, True)
+        llik = scorecard['_frames']['llik']
+
+        llik['llik_model'] = Strategy._log_likelihood(llik['selection_won'], llik['u_p'])
+        scorecard['llik']['model'] = llik['llik_model'].sum()
+
+        print(llik)
+
+        if not llik_frame:
+            del scorecard['_frames']
+        return scorecard
 
     def handle_race(self, race):
         if race['event'] == TO_BE_PLACED or race['n_runners'] < 3:
@@ -137,30 +164,33 @@ class Jockey(Strategy):
 
         runners = race['selection']
         # self.total_matched(race['event_id']) > 2e5
-        if np.all(self.hm.get_runs(runners) > 1):
+        if np.all(self.hm.get_runs(runners) > 0):
             vwao = self.vwao.ix[race['event_id']]['vwao'][runners].values
-            #q = 1.0 / vwao / np.sum(1.0 / vwao)
-            q = 1.0 / vwao
+            implied = self.vwao.ix[race['event_id']]['implied'][runners].values
             p = self.hm.pwin_trapz(runners)
 
-            rel = p / q - 1.0
+            rel = p / implied - 1.0
             t = 0.1
 
-            p[rel < -t] = q[rel < -t] * 0.9
-            p[rel > t] = q[rel > t] * 1.1
+            #p[rel < -t] = implied[rel < -t] * 0.9
+            #p[rel > t] = implied[rel > t] * 1.1
 
             #print(p)
             # ps = (self.hm.get_runs(runners) * p + 4 * q) / (4 + self.hm.get_runs(runners))
 
             #w = RiskModel2(p, q).optimal_w()
-            w = risk.nwin1_l2reg(p, q, 0.1)
+            w = risk.nwin1_l2reg(p, vwao, 0.1)
 
-            coll = np.min(risk.nwin1_bet_returns(w, 1 / q))
-            if coll > -60:
-                logging.info('Placing some bets: %.2f' % np.sum(np.abs(w)))
-                [self.bet(race['event_id'], r, w[i], {'p': p[i]}) for i, r in enumerate(runners)]
-            else:
-                logging.info('Skipping placing bets as coll=%.2f' % coll)
+            returns = risk.nwin1_bet_returns(w, vwao)
+            ix = np.where(returns < -50)[0]
+            if False and len(ix) > 0:
+                for i in ix:
+                    logging.info('Zeroing bet of %.2f on "%s" with potential returns = %.2f' %
+                                 (w[i], runners[i], returns[i]))
+                    w[i] = 0.0
+
+            logging.info('Placing some bets: %.2f' % np.sum(np.abs(w)))
+            [self.bet(race['event_id'], r, w[i], {'p': p[i]}) for i, r in enumerate(runners) if np.abs(w[i]) > 0.01]
 
         self.hm.fit_race(race)
 
@@ -177,14 +207,16 @@ if __name__ == '__main__':
     configure_root_logger(True)
 
     db = MongoClient(port=30001)['betfair']
-    algo = Jockey(db, train='train_hrb')
+    algo = Balius(db, train='train_hrb')
 
     st = time.clock()
-    algo.run('GB') # , start_date=datetime.datetime(2012, 1, 1), end_date=datetime.datetime(2013, 1, 1))
+    algo.run('GB' , start_date=datetime.datetime(2012, 1, 1), end_date=datetime.datetime(2012, 2, 1))
     en = time.clock()
 
+    db['scorecards'].insert(algo.make_scorecard())
+
     df = pd.DataFrame.from_dict(algo._bets)
-    print(df.to_string())
+    # print(df.to_string())
     print('Done in %.4f s' % (en - st))
 
     df.save('/home/marius/playground/btrading/back7.pd')
