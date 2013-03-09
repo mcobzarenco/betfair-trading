fixed model likelihood calculation (it depends on the strategy to provide the implied probability)

harb/scorecard.py
@@ -57,9 +57,8 @@ def make_scorecard(bets, percentile_width=60, comm=DEFAULT_COMM, jsonify=True, l
 
     user_columns = bets.filter(regex='data_.*').columns.tolist()
     llik = bets.groupby(['market_id', 'selection'])[['selection_won', 'n_runners'] + user_columns].last()
-    #llik['llik_implied'] = np.log(llik['implied'][llik['selection_won'] == 1])
-    #llik['llik_implied'].fillna(0.0, inplace=True)
-    llik['llik_implied'] = 0.0
+    llik['llik_model'] = np.log(llik['data_implied'][llik['selection_won'] == 1])
+    llik['llik_model'].fillna(0.0, inplace=True)
     llik['llik_uniform'] = np.log(1.0 / bets['n_runners'])
     llik['llik_model'] = np.log(llik['data_p'][llik['selection_won'] == 1])
     llik['llik_model'].fillna(0.0, inplace=True)

harb/strategy.py
@@ -14,6 +14,7 @@ from analytics import HorseModel, DEFAULT_MU, DEFAULT_SIGMA, DEFAULT_BETA, DEFAU
     get_implied_from_odds
 import risk
 
+
 WARN_LIQUIDITY = 0.2
 DEFAULT_COMM = 0.95
 LOGGING_NRACES = 500
@@ -76,8 +77,13 @@ class Balius(object):
             else:
                 logging.info('Betting on market_id=%s: |exposure|=%.2f collateral=%.2f' %
                              (race['market_id'], np.sum(np.abs(w)), np.min(returns)))
-                bets = ({'selection_id': prices[r]['selection_id'], 'amount': w[i], 'data': {'p': p[i]}}
-                        for i, r in enumerate(runners))
+                bets = ({'selection_id': prices[r]['selection_id'],
+                         'amount': w[i],
+                         'data': {
+                             'p': p[i],
+                             'implied': implied[i]
+                         }
+                        } for i, r in enumerate(runners))
                 ex.place_exchange_bets(race['market_id'], bets)
 
         if 'ranking' in race: